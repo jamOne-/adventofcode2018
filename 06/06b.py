1799a4b6d9dd828ab63ec1b21d059be9705f71d6
jamone- <dominik.rabij@gmail.com>
2018-12-06 22:29:10 +0100

    06 there's only one region...

--- 06/06b.py
+++ 06/06b.py
@@ -29,29 +29,7 @@ def get_neighbours(point):
 def solve(lines):
     points = list(map(lambda line: tuple(map(int, line.split(', '))), lines))
     safe_points = set(get_safe_points(points))
-    visited_points = set()
-
-    largest = 0
-
-    for x, y in safe_points:
-        if (x, y) in visited_points:
-            continue
-
-        current = 0
-        stack = [(x, y)]
-
-        while stack:
-            x, y = stack.pop()
-
-            for x, y in get_neighbours((x, y)):
-                if (x, y) in safe_points and (x, y) not in visited_points:
-                    current += 1
-                    stack.append((x, y))
-                    visited_points.add((x, y))
-
-        largest = max(largest, current)
-
-    return largest
+    return len(safe_points)
 
 
 print(solve(sys.stdin))
